f0bac09db541c5d908fdb24178adb9030ef701cc
mobin alavi <mobinalavynasab@gmail.com>
2023-05-08 10:38:26 +0430

    first commit

---
+++ reverse string/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace reverse_string.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/<ValuesController>/5
+        [HttpGet("{input}")]
+        public string Get(string input)
+        {
+            return input;
+        }
+    }
+}
---
+++ reverse string/Controllers/reverseClass.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace reverse_string.Controllers
+{
+    [ApiController]
+    [Route("api/[controller],[action]")]
+    public class reverseClass : ControllerBase
+    {
+        [HttpGet]
+        public string num(string input)
+        {
+            char[] output = input.ToCharArray();
+            Array.Reverse(output);
+            string reversename = new string(output);
+            return reversename;
+        }
+    }
+}
